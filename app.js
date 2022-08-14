acb8f0145228db69c16e3960f8c3c1497c42377e
Laskellyb <anikwenzekelly@gmail.com>
2022-08-14 09:33:50 +0100

    return and borrow book function plus set timeout complete

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ function checkComplete () {
 checkComplete()
 
 // Reduce quantity when borrowed
+const borrowSuccessMsg = document.querySelector('.borrow-success-msg');
+const returnSuccessMsg = document.querySelector('.return-success-msg');
 
 // Borrow book event listener
 borrowBookBtn.addEventListener('click', ()=> {
@@ -66,13 +68,17 @@ borrowBookBtn.addEventListener('click', ()=> {
         // bookQty.innerText = newBookQty
 
         if (bookQty.innerText < 1){
-          // Book has finished
+          // Conditional if Book has finished
           alert('Book currently not available')
         } else {
            bookQty.innerText = bookQty.innerText - 1;
+          //  Display success and remove after few seconds
+          borrowSuccessMsg.classList.remove('display-none')
+           setTimeout(()=> {
+            borrowSuccessMsg.classList.add('display-none')
+           }, 2000)
+          searchBooksInput.value = '';
         }
-        console.log(individualBookName)
-        console.log(bookQty)
       }
   });
 })
@@ -95,9 +101,50 @@ returnBookBtn.addEventListener('click', ()=> {
           alert('Book currently not available')
         } else {
            bookQty.innerText = parseInt(bookQty.innerText) + 1;
+           //  Display success and remove after few seconds
+          returnSuccessMsg.classList.remove('display-none')
+           setTimeout(()=> {
+            returnSuccessMsg.classList.add('display-none')
+           }, 2000)
+           searchBooksInput.value = '';
         }
         console.log(individualBookName)
         console.log(bookQty)
       }
   });
 })
+
+// trying to make a function for both
+
+// // Borrow and return book function
+// function borrowReturnBooks(successmsg, value, num){
+//   bookName.forEach(name => {
+//     const individualBookName = name.innerText;
+
+//     // Variable to get each quantity
+//     let bookQty = name.nextElementSibling.nextElementSibling
+
+//     // Check if search input is equals any individualBookName and minus quantity
+//       if(searchBooksInput.value === individualBookName){
+//         // newBookQty = bookQty.innerText - 1;
+//         // bookQty.innerText = newBookQty
+
+//         if (bookQty.innerText < 1){
+//           // Conditional if Book has finished
+//           alert('Book currently not available')
+//         } else {
+//            bookQty.innerText = value.innerText ${num} 1;
+//           //  Display success and remove after few seconds
+//           successmsg.classList.remove('display-none')
+//            setTimeout(()=> {
+//             successmsg.classList.add('display-none')
+//            }, 2000)
+//         }
+//       }
+//   });
+// }
+
+
+// borrowBookBtn.addEventListener('click', (borrowSuccessMsg, bookQty.innerText - 1 )=> {
+  
+// })
